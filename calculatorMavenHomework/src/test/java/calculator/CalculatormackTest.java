c04b9872777f823dc923280557128611ca7bba13
bethani <wendifraw15@gmail.com>
2020-04-05 19:29:58 -0700

    mack Regrassion Test

---
+++ calculatorMavenHomework/src/test/java/calculator/CalculatormackTest.java
@@ -0,0 +1,78 @@
+package calculator;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CalculatormackTest {
+
+  @Test
+  public void sumTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(3, 6), 9);
+  }
+  
+
+  @Test
+  public void keyaTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(4, 6), 10);
+  }
+  
+
+  @Test
+  public void anduAlemTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(12, 6), 18);
+  }
+  
+
+  @Test
+  public void mahaletTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(3, 2), 5);
+  }
+  
+
+  @Test
+  public void azebTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(2, 4), 6);
+  }
+  
+  
+
+  @Test
+  public void eyerusalemTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(10, 6), 16);
+  }
+  
+
+  @Test
+  public void mackTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(1, 6), 7);
+  }
+  
+
+  @Test
+  public void meronTest() {
+    Calculator calculator = new Calculator();
+    assertEquals(calculator.sum(3, 7), 10);
+  }
+  
+
+  @Test
+  public void MackTest4() {
+  
+    System.setProperty("webdriver.chrome.driver",
+			"C:\\Users\\Wendifraw\\Downloads\\chromedriver_win32 (2)\\chromedriver.exe");
+
+	WebDriver driver = new ChromeDriver();
+
+	 driver.get("http://api.icraftsoft.net/register/#!");
+  }
+}
